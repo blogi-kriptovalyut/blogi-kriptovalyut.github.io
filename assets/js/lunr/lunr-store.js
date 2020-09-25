936170425bbd9b6ea2ac13c6fae884e403365b40
dev <user@users.noreply.github.com>
2020-09-25 22:02:01 +0300

    ja

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2242,4 +2242,10 @@ var store = [{
         "tags": ["история"],
         "url": "https://bitkoinblog.ru/news/gen-minfin-09-24/",
         "teaser": "https://bitkoinblog.ru/assets/images/thumbs/500x300.png"
+      },{
+        "title": "Крах Фондовых рынков продолжится. Дно еще впереди",
+        "excerpt":"**    ","categories": ["News"],
+        "tags": ["история"],
+        "url": "https://bitkoinblog.ru/news/gen-clear-09-25/",
+        "teaser": "https://bitkoinblog.ru/assets/images/thumbs/500x300.png"
       },]
